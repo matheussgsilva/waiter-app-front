91f2dedc963d7672cc07464e862d2fee64832b47
Matheus Silva <matheussgsilva@hotmail.com>
2024-10-31 21:43:37 -0300

    start project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
+export function App() {
+  return (
+    <>
+    <GlobalStyles />
+    <Header />
+    </>
+);
+}
